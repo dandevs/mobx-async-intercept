1acdb4a581eec4da03999f2c61c302e4f2c824b8
dandevs <danilodevs@gmail.com>
2018-09-16 19:59:05 -0400

    Ignore dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IObjectWillChange } from "mobx";
-export declare function interceptAsync(target: any, handler: (change: IObjectWillChange) => Promise<HandlerOutput>): void;
-declare type HandlerOutput = IObjectWillChange | null;
-export {};
